75b328ae6ae9535433ec3f6369ab37e00cded570
Kumar Kartikeya <kartikeya.dhoni@gmail.com>
2020-11-11 13:36:23 +0530

    [Kartikeya] UC added to add data to multiple tables in database

--- EmployeePayroll_Threading/EmployeePayrollOperations.cs
+++ EmployeePayroll_Threading/EmployeePayrollOperations.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -17,6 +18,11 @@ namespace EmployeePayroll_Threading
         /// </summary>
         public Mutex mutex = new Mutex();
         /// <summary>
+        /// Setting up the connection with the database
+        /// </summary>
+        public static string connectionString = "Data Source=KARTIKEYA;Initial Catalog=payroll_service;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+        SqlConnection connection = new SqlConnection(connectionString);
+        /// <summary>
         /// UC 1:
         /// Adding employee details to the list
         /// </summary>
@@ -26,7 +32,7 @@ namespace EmployeePayroll_Threading
             employeePayrollDataList.ForEach(employeeData =>
             {
                 Console.WriteLine(" Employee being added: " + employeeData.EmpName);
-                this.addEmployeePayroll(employeeData);
+                this.AddEmployeePayroll(employeeData);
                 Console.WriteLine(" Employee added: " + employeeData.EmpName);
             });
             Console.WriteLine(this.employeePayrollDetailList.Count);
@@ -44,7 +50,7 @@ namespace EmployeePayroll_Threading
                 Task thread = new Task(() =>
                 {
                     Console.WriteLine(" Employee being added: " + employeeData.EmpName);
-                    this.addEmployeePayroll(employeeData);
+                    this.AddEmployeePayroll(employeeData);
                     Console.WriteLine(" Employee added: " + employeeData.EmpName);
                 });
                 thread.Start();
@@ -64,7 +70,7 @@ namespace EmployeePayroll_Threading
                   {
                       mutex.WaitOne();
                       Console.WriteLine("Employee Being added" + employeeData.EmpName);
-                      this.addEmployeePayroll(employeeData);
+                      this.AddEmployeePayroll(employeeData);
                       Console.WriteLine("Employee added:" + employeeData.EmpName);
                       Console.WriteLine(Thread.CurrentThread.ManagedThreadId);
                       mutex.ReleaseMutex();
@@ -72,10 +78,50 @@ namespace EmployeePayroll_Threading
                 thread.Start();
             });
         }
-
-        public void addEmployeePayroll(EmployeeDetails emp)
+        public void AddEmployeePayroll(EmployeeDetails emp)
         {
             employeePayrollDetailList.Add(emp);
+        }
+        /// <summary>
+        /// UC 5:
+        /// Adding employee payroll data to multiple tables 
+        /// </summary>
+        /// <param name="employeeDetails"></param>
+        /// <returns></returns>
+        public bool AddMultipleEmployeesToDatabase(EmployeeDetails employeeDetails)
+        {
+            try
+            {
+                using (this.connection)
+                {
+                    SqlCommand command = new SqlCommand("dbo.spAddMultipleEmployees", this.connection);
+                    command.CommandType = System.Data.CommandType.StoredProcedure;
+                    command.Parameters.AddWithValue("@CompanyId", employeeDetails.CompanyId);
+                    command.Parameters.AddWithValue("@EmpId", employeeDetails.EmpID);
+                    command.Parameters.AddWithValue("@EmpName", employeeDetails.EmpName);
+                    command.Parameters.AddWithValue("@Gender", employeeDetails.Gender);
+                    command.Parameters.AddWithValue("@PhoneNo", employeeDetails.PhoneNo);
+                    command.Parameters.AddWithValue("@Address", employeeDetails.Address);
+                    command.Parameters.AddWithValue("@StartDate", employeeDetails.StartDate);
+                    command.Parameters.AddWithValue("@BasicPay", employeeDetails.BasicPay);
+                    command.Parameters.AddWithValue("@Deductions", employeeDetails.Deductions);
+                    command.Parameters.AddWithValue("@TaxablePay", employeeDetails.TaxablePay);
+                    command.Parameters.AddWithValue("@IncomeTax", employeeDetails.Tax);
+                    command.Parameters.AddWithValue("@NetPay", employeeDetails.NetPay);
+                    connection.Open();
+                    var result = command.ExecuteNonQuery();
+                    connection.Close();
+                    if (result != 0)
+                    {
+                        return true;
+                    }
+                    return false;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
         }
     }
 }
